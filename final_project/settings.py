5ca8190b8cd6ec54c03ec3a4b06d62e426776467
Alexey <nafania217518@gmail.com>
2025-10-27 21:51:39 +0200

    uhm regiter/login/logout added

--- final_project/settings.py
+++ final_project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'internet_shop'
+    'auth_app',
+    'internet_shop',
+    
 ]
 
 MIDDLEWARE = [
